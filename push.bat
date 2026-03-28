d5361abdcc726585cedb1a33f26670403e66bcac
Sandhyasakthi <sandhyasakunthala4@gmail.com>
2026-03-28 20:28:43 +0530

    Update push.bat

--- push.bat
+++ push.bat
@@ -4,9 +4,9 @@ git commit -m "Deployment ready, Kaggle dataset integration, and UI update"
 git branch -M main
 git remote | findstr origin >nul
 if %errorlevel% equ 0 (
-    git remote set-url origin https://github.com/Sandhyasakthi/customer_satisfaction
+    git remote set-url origin https://github.com/Sandhyasakthi/Ai-Customer-Satisfaction-prediction.git
 ) else (
-    git remote add origin https://github.com/Sandhyasakthi/customer_satisfaction
+    git remote add origin https://github.com/Sandhyasakthi/Ai-Customer-Satisfaction-prediction.git
 )
 echo Pushing to GitHub...
 git push -u origin main
